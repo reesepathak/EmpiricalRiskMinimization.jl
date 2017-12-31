5aeb02a5328c051af8027b3a77bb91e8ed42ae34
Reese Pathak <rpathak@stanford.edu>
2017-12-30 18:47:51 -0800

    added L2Reg

--- src/regularizers.jl
+++ src/regularizers.jl
@@ -13,3 +13,18 @@ function prox(R::L1Reg, grad_step, t)
     scale = R.weight*t
     return sign.(grad_step).*max.(0, abs.(grad_step) - scale)
 end
+
+#########################################
+# L2 Regularizer
+#########################################
+struct L2Reg <: Regularizer
+    weight::Float64
+end
+
+L2Reg() = L2Reg(1.0)
+
+function prox(R::L2Reg, grad_step, t)
+    scale = R.weight*t
+    norm = norm(grad_step)
+    return (norm >= scale) ? (1 - scale/norm)*grad_step : zeros(length(grad_step))
+end
