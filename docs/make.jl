fa50db36c7ed2bd9d66522820101a6d3453781ca
Reese Pathak <rpathak@stanford.edu>
2018-04-01 17:01:14 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ using Documenter, EmpiricalRiskMinimization
 makedocs(modules=[EmpiricalRiskMinimization], doctest=true)
 
 deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"), 
-	   repo = "github.com/reesepathak/EmpiricalRiskMinimization.jl", 
+	   repo = "github.com/reesepathak/EmpiricalRiskMinimization.jl.git", 
 	   julia = "0.6.2", 
 	   osname = "linux")
