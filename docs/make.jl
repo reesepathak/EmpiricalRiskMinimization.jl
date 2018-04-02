7569739661b307f6d8a7b3b2e0d4dea18e67b69a
reesepathak <rpathak@stanford.edu>
2018-04-01 17:19:15 -0700

    Documenter change - docs will be made by travis linux bot running julia v0.6

--- docs/make.jl
+++ docs/make.jl
@@ -4,5 +4,5 @@ makedocs(modules=[EmpiricalRiskMinimization], doctest=true)
 
 deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"), 
 	   repo = "github.com/reesepathak/EmpiricalRiskMinimization.jl.git", 
-	   julia = "0.6.2", 
+	   julia = "0.6", 
 	   osname = "linux")
