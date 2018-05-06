662fc7dd89f153a7c9109e3c2ad3cdba8b4e6c44
autodocs <autodocs>
2018-05-06 21:53:09 +0000

    build based on bb9ad7a

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,93 +21,133 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Getting Started",
     "category": "section",
-    "text": "For now, the package should be installed using the following line of code.Pkg.clone(\"https://github.com/reesepathak/EmpiricalRiskMinimization.jl.git\")\nPkg.update()Although the package has been submitted to the official Julia repository, if you include it using Pkg.add() or with one of the other standard installation methods, you will not recieve the most recent version of the code. The code is very volatile right now, so it is essential that users Pkg.update() prior to using the code.Then head over to [usage/introduction the first examples page] to begin using ERM.jl!"
+    "text": "For now, the package should be installed using the following line of code.Pkg.clone(\"https://github.com/reesepathak/EmpiricalRiskMinimization.jl.git\")\nPkg.update()Although the package has been submitted to the official Julia repository, if you include it using Pkg.add() or with one of the other standard installation methods, you will not recieve the most recent version of the code. The code is very volatile right now, so it is essential that users Pkg.update() prior to using the code.Then head over to our ERM walkthrough to begin using ERM.jl."
 },
 
 {
-    "location": "usage/introduction.html#",
-    "page": "-",
-    "title": "-",
+    "location": "examples/walkthrough.html#",
+    "page": "Walkthrough",
+    "title": "Walkthrough",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "usage/installation.html#",
-    "page": "-",
-    "title": "-",
+    "location": "examples/walkthrough.html#Walkthrough-1",
+    "page": "Walkthrough",
+    "title": "Walkthrough",
+    "category": "section",
+    "text": "This page gives an overview of how to use EmpiricalRiskMinimization.jl on a basic dataset. "
+},
+
+{
+    "location": "examples/additional_examples.html#",
+    "page": "Usage examples",
+    "title": "Usage examples",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "examples/index.html#",
-    "page": "-",
-    "title": "-",
+    "location": "examples/additional_examples.html#Usage-examples-1",
+    "page": "Usage examples",
+    "title": "Usage examples",
+    "category": "section",
+    "text": "The links below reference IPython notebooks where you can see how to apply EmpiricalRiskMinimization.jl for various (increasingly complex) use cases."
+},
+
+{
+    "location": "usage/modelling.html#",
+    "page": "ERM models",
+    "title": "ERM models",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "functions/losses.html#",
-    "page": "-",
-    "title": "-",
+    "location": "usage/modelling.html#ERM-models-1",
+    "page": "ERM models",
+    "title": "ERM models",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "usage/losses.html#",
+    "page": "Losses",
+    "title": "Losses",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "functions/regularizers.html#",
-    "page": "-",
-    "title": "-",
+    "location": "usage/losses.html#Losses-1",
+    "page": "Losses",
+    "title": "Losses",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "usage/regularizers.html#",
+    "page": "Regularizers",
+    "title": "Regularizers",
     "category": "page",
+    "text": ""
+},
+
+{
+    "location": "usage/regularizers.html#Regularizers-1",
+    "page": "Regularizers",
+    "title": "Regularizers",
+    "category": "section",
     "text": ""
 },
 
 {
     "location": "lib/losses.html#",
-    "page": "Loss functions",
-    "title": "Loss functions",
+    "page": "Losses",
+    "title": "Losses",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "lib/losses.html#EmpiricalRiskMinimization.SquareLoss",
-    "page": "Loss functions",
+    "page": "Losses",
     "title": "EmpiricalRiskMinimization.SquareLoss",
     "category": "type",
     "text": "SquareLoss()\n\nConstructs the squared loss or l2 loss.\n\n\n\n"
 },
 
 {
-    "location": "lib/losses.html#Loss-functions-1",
-    "page": "Loss functions",
-    "title": "Loss functions",
+    "location": "lib/losses.html#Losses-1",
+    "page": "Losses",
+    "title": "Losses",
     "category": "section",
     "text": "CurrentModule = EmpiricalRiskMinimizationModules = [EmpiricalRiskMinimization]\nOrder = [:type, :function]\nPages = [\"losses.jl\"]"
 },
 
 {
     "location": "lib/modeling.html#",
-    "page": "Models and Training",
-    "title": "Models and Training",
+    "page": "Modelling and training",
+    "title": "Modelling and training",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "lib/modeling.html#EmpiricalRiskMinimization.status-Tuple{EmpiricalRiskMinimization.Model}",
-    "page": "Models and Training",
+    "page": "Modelling and training",
     "title": "EmpiricalRiskMinimization.status",
     "category": "method",
     "text": "status(M) Prints and returns the status of the ERM model, M. Will only  print the most recent action (e.g., training, regularization path, etc.)  performed on the model. \n\n\n\n"
 },
 
 {
-    "location": "lib/modeling.html#Models-and-Training-1",
-    "page": "Models and Training",
-    "title": "Models and Training",
+    "location": "lib/modeling.html#Modelling-and-training-1",
+    "page": "Modelling and training",
+    "title": "Modelling and training",
     "category": "section",
     "text": "CurrentModule = EmpiricalRiskMinimizationModules = [EmpiricalRiskMinimization]\nOrder = [:type, :function]\nPages = [\"model.jl\"]"
 },
